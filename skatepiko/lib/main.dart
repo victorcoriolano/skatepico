fbb6b324648ef2b19e411a54382033ba3d23578e
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-08-01 16:26:41 -0300

    first commit

--- skatepiko/lib/main.dart
+++ skatepiko/lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:skatepiko/page/pagina.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/page/pagina.dart',
+        builder: (BuildContext context, GoRouterState state) => const HomePage(),
+      ),
+    ],
+  );
   runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+      routes:  {
+        '/page/pagina.dart': (context) => const HomePage(),
+      },
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/page/pagina.dart',
+      );
   }
 }
